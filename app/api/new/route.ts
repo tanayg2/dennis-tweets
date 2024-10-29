6b34ca166eef238ddaca8eb000a6ae760fc54629
Tanay Gandhi <tgandhi.dev@gmail.com>
2024-10-29 10:04:48 -0700

    logs

--- app/api/new/route.ts
+++ app/api/new/route.ts
@@ -5,46 +5,39 @@ import { createClient } from "@supabase/supabase-js"
 export async function POST(request: Request) {
   const supabase = createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SERVICE_KEY!
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
   )
 
   // read data from request body (type readable stream)
   const data = await request.json()
-  console.log("here")
 
   // if (data.query) {
   //   console.log("Query params: ", data.query)
   // }
 
   const url = data.query.text ?? data.body.text
-  console.log("URL: ", url)
   if (!url) {
     return Response.json("No link found in data", { status: 400 })
   }
   try {
-    console.log("here2")
     new URL(url)
   } catch (e) {
     return Response.json(`Invalid URL: ${url}`, { status: 400 })
   }
 
-  console.log("here3")
   const type = getPostType(url)
   if (!type) {
     return Response.json(`Invalid URL ${url}`, { status: 400 })
   }
 
-  console.log("here4")
   const owner = data.query.msisdn ?? data.body.from
   const newPost: Database["public"]["Tables"]["posts"]["Insert"] = {
     owner,
     url,
     type,
   }
-  console.log("Data: ", newPost)
 
   try {
-    console.log("here5", newPost)
     const res = await supabase.from("posts").insert(newPost)
     return Response.json(res, { status: 200 })
   } catch (e) {
