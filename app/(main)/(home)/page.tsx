2a7568a5ce6e7cfef361569aa488399459b710d0
Tanay Gandhi <tgandhi.dev@gmail.com>
2024-12-26 12:15:27 -0800

    Update page.tsx

--- app/(main)/(home)/page.tsx
+++ app/(main)/(home)/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react"
 import { LoadMore } from "./_components/LoadMore"
 
 export default async function Page() {
   return (
     <div className="space-y-8">
-      <LoadMore />
+      <Suspense fallback={null}>
+        <LoadMore />
+      </Suspense>
     </div>
   )
 }
