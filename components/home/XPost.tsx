dedfc5978ee993b6a067c6f8fe1242759432c1a9
Tanay Gandhi <tgandhi.dev@gmail.com>
2024-12-28 20:52:04 -0800

    rm unused files

--- components/home/XPost.tsx
+++
@@ -1,9 +0,0 @@
-import { Database } from "@/lib/db.types"
-import { XEmbed } from "react-social-media-embed"
-
-export const XPost = (props: {
-  post: Database["public"]["Views"]["enriched_posts"]["Row"]
-}) => {
-  if (!props.post.url) return null
-  return <XEmbed url={props.post.url} />
-}
